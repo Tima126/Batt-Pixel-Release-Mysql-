53d020043ddf5eadd9621537d111b4fb3659d28a
Tima126 <148056216+Tima126@users.noreply.github.com>
2024-12-08 23:02:51 +0300

    Server http

--- BatPixel/public/Pain.js
+++ BatPixel/public/Pain.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const zoomInButton = document.getElementById('zoomInButton');
     const zoomOutButton = document.getElementById('zoomOutButton');
 
-    const socket = new WebSocket('ws://localhost');
+    const socket = new WebSocket('wss://batt-pixel-release-mysql-3.onrender.com/');
 
     const pixelSize = 10; 
     const drawCooldown = 10000; 
